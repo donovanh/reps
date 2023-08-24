7782b3ceb23a960734c17f184679e307a4a8e377
Donovan Hutchinson <donovanh@gmail.com>
2023-08-24 21:41:32 +0100

    fix: Adding animation for football prop when available

--- Reps/Views/Workout/AnimationView.swift
+++ Reps/Views/Workout/AnimationView.swift
@@ -25,13 +25,24 @@ struct AnimationView: View {
         let animationSceneSource = SCNSceneSource(url: animationUrl!, options: nil)
         
         let animationIdentifier = "action_container-rig"
+        let footballAnimationIdentifier = "football2_ball_football2_ballAction_transform"
         let cameraIdentifier = "Camera"
         
-        // Load animation from source
+        // Load dummy animation from source
         if let animationObj = animationSceneSource?.entryWithIdentifier(animationIdentifier,
                                                          withClass: CAAnimation.self) {
+            print("Animation rig")
             animationObj.repeatCount = .infinity
-            scene.rootNode.addAnimation(animationObj, forKey: "action_container-rig")
+            scene.rootNode.addAnimation(animationObj, forKey: animationIdentifier)
+        }
+        
+        // Load animation for football if found
+        // TODO: Remake this something tidy
+        if let footballAnimationObj = animationSceneSource?.entryWithIdentifier(footballAnimationIdentifier,
+                                                         withClass: CAAnimation.self) {
+            print("Applying football animation")
+            footballAnimationObj.repeatCount = .infinity
+            scene.rootNode.addAnimation(footballAnimationObj, forKey: footballAnimationIdentifier)
         }
         
         // Override camera position
